39b10fd5d003a39d6a03ed8e19ebb3c48fdf78ec
Jason Layton <16jalayt@gmail.com>
2023-05-08 15:44:34 -0500

    Rewrote avf again to use sdl texture. The correct way

--- source/Nancy/AVF.h
+++ source/Nancy/AVF.h
@@ -9,7 +9,9 @@ public:
 	static std::vector<SDL_Texture_ptr> parseAVF(const char* file);
 
 private:
-	static std::vector<uint8_t> createBMPHeader(int length, int width, int height);
-	static SDL_Texture* AVFPNG(std::vector<uint8_t> frame, int width, int height);
-	static SDL_Texture* AVFBMP(std::vector<uint8_t> frame, int width, int height);
 };
+
+SDL_Texture* AVFPNG(std::vector<uint8_t> frame, int width, int height);
+//SDL_Texture* AVFBMP(std::vector<uint8_t> frame, int width, int height);
+//std::vector<uint8_t> createBMPHeader(int length, int width, int height);
+SDL_Texture* RGB555_888_SDL(std::vector<uint8_t> col555, int width, int height);
--- source/oldhertools/colorspace.cpp
+++ source/oldhertools/colorspace.cpp
@@ -2,6 +2,7 @@
 
 #include "colorspace.hpp"
 
+//Might not be 555, ARGB1555 works
 namespace Her {
 	std::vector<uint8_t> RGB555_888(std::vector<uint8_t> col555) {
 		std::vector<uint8_t> col888;
@@ -81,50 +82,39 @@ namespace Her {
 
 	/*void RGB555_888_SDL(std::vector<uint8_t> col555, SDL_Texture*& texture) {
 		Uint8 colR;
-		Uint8 colG;
-		Uint8 colB;
+	Uint8 colG;
+	Uint8 colB;
 
-		Uint32* pixels = nullptr;
-		int pitch = 0;
-		Uint32 format;
+	unsigned char* pixels = nullptr;
+	int pitch = 0;
+	SDL_Texture* texture = SDL_CreateTexture(Graphics::renderer.get(), SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, width, height);
 
-		// Get the size of the texture.
-		int w, h;
-		SDL_QueryTexture(texture, &format, nullptr, &w, &h);
+	// Now let's make our "pixels" pointer point to the texture data.
+	if (SDL_LockTexture(texture, nullptr, (void**)&pixels, &pitch))
+	{
+		// If the locking fails, you might want to handle it somehow. SDL_GetError(); or something here.
+		printf("fail");
+	}
 
-		// Now let's make our "pixels" pointer point to the texture data.
-		if (SDL_LockTexture(texture, nullptr, (void**)&pixels, &pitch))
-		{
-			// If the locking fails, you might want to handle it somehow. SDL_GetError(); or something here.
-			printf("fail");
-		}
+	int j = 0;
+	for (unsigned int i = 0; i < col555.size(); i += 2) {
+		colR = (col555.at(i + 1) & 0x7C) << 1;
+		colG = (col555.at(i + 1) & 0x03) << 6 | (col555.at(i) & 0xE0) >> 2;
+		colB = (col555.at(i) & 0x1F) << 3;
 
-		SDL_PixelFormat pixelFormat;
-		pixelFormat.format = format;
+		colR |= colR >> 5;
+		colG |= colG >> 5;
+		colB |= colB >> 5;
 
-		for (unsigned int i = 0; i < col555.size(); i += 2) {
-			colR = (col555.at(i + 1) & 0x7C) << 1;
-			colG = (col555.at(i + 1) & 0x03) << 6 | (col555.at(i) & 0xE0) >> 2;
-			colB = (col555.at(i) & 0x1F) << 3;
+		pixels[j] = colR;
+		pixels[j + 1] = colG;
+		pixels[j + 2] = colB;
+		j += 3;
+	}
 
-			colR |= colR >> 5;
-			colG |= colG >> 5;
-			colB |= colB >> 5;
+	// Also don't forget to unlock your texture once you're done.
+	SDL_UnlockTexture(texture);
 
-			// Now you want to format the color to a correct format that SDL can use.
-		// Basically we convert our RGB color to a hex-like BGR color.
-			Uint32 color = SDL_MapRGB(&pixelFormat, 50, 50, 50);
-
-			// Before setting the color, we need to know where we have to place it.
-			Uint32 pixelPosition = (i / 3 / w) * (pitch / sizeof(unsigned int)) + (3 % w);
-
-			// Now we can set the pixel(s) we want.
-			pixels[pixelPosition] = color;
-		}
-
-		// Also don't forget to unlock your texture once you're done.
-		SDL_UnlockTexture(texture);
-
-		return;
+	return texture;
 	}*/
 }
