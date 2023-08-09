277024e2e0a551f11433ca54edf63ab39184f87f
Jason Layton <16jalayt@gmail.com>
2023-08-08 21:07:12 -0500

    Add more cursors and fix logic

--- source/Engine/Cursor.cpp
+++ source/Engine/Cursor.cpp
@@ -3,18 +3,20 @@
 #include <SDL2/SDL_image.h>
 #include "utils.h"
 
-SDL_Cursor_ptr Cursor::cursors[20];
+SDL_Cursor_ptr Cursor::cursors[30];
 int Cursor::currentCursor = 0;
 int Cursor::baseCursor = 0;
 bool Cursor::CursorChanged = false;
+SDL_Surface_ptr Cursor::spriteSheet;
+std::string Cursor::spriteSheetPath;
 
-SDL_Cursor_ptr loadCursorFromSheet(std::string filename, SDL_Rect rect)
+SDL_Cursor_ptr Cursor::loadCursorFromSheet(std::string filename, SDL_Rect rect)
 {
-	//TODO: make genaric as sheet, and make array version that doesn't dispose of sheet
-	SDL_Surface* spriteSheet = IMG_Load(filename.c_str());
+	if (spriteSheetPath != filename)
+		spriteSheet = SDL_Surface_ptr(IMG_Load(filename.c_str()));
 	if (!spriteSheet)
 	{
-		LOG_F(ERROR, "Unable to load cursor Sprite Sheet.\n");
+		LOG_F(ERROR, "Unable to load cursor Sprite Sheet: %s.\n", filename.c_str());
 		return NULL;
 	}
 
@@ -26,9 +28,9 @@ SDL_Cursor_ptr loadCursorFromSheet(std::string filename, SDL_Rect rect)
 		return NULL;
 	}
 
-	SDL_BlitSurface(spriteSheet, &rect, sprite, NULL);
+	SDL_BlitSurface(spriteSheet.get(), &rect, sprite, NULL);
 
-	SDL_FreeSurface(spriteSheet);
+	SDL_FreeSurface(spriteSheet.get());
 
 	SDL_Cursor_ptr cursor = SDL_Cursor_ptr(SDL_CreateColorCursor(sprite, 10, 10));
 
@@ -37,7 +39,7 @@ SDL_Cursor_ptr loadCursorFromSheet(std::string filename, SDL_Rect rect)
 
 int Cursor::Init()
 {
-	//TODO: make default placeholder
+	//TODO: make default placeholder, bug with random starting cursor
 
 	//Default Magnifying
 	cursors[0] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 506, 3, 30, 40 }));
@@ -45,10 +47,21 @@ int Cursor::Init()
 	cursors[1] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 537, 3, 30, 40 }));
 	//Left Arrow
 	cursors[6] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 568, 167, 30, 40 }));
+	//Right Arrow
+	cursors[7] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 568, 208, 30, 40 }));
+	//Back Arrow
+	cursors[9] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 506, 290, 30, 40 }));
 	//Uturn Arrow
 	cursors[14] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 568, 85, 30, 40 }));
 	//Forward Arrow
 	cursors[15] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 475, 290, 30, 40 }));
+	//Back Arrow (duplicate)
+	cursors[16] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 506, 290, 30, 40 }));
+	//Left Arrow (duplicate)
+	cursors[22] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 568, 167, 30, 40 }));
+	//Right Arrow (duplicate)
+	cursors[23] = std::move(loadCursorFromSheet("DataFiles/CIFTREE/OBJECT0.png", SDL_Rect{ 568, 208, 30, 40 }));
+
 	return 0;
 }
 
@@ -56,20 +69,23 @@ void Cursor::DrawCursor()
 {
 	if (!CursorChanged)
 		currentCursor = baseCursor;
+
+	if (!cursors[currentCursor])
+	{
+		LOG_F(ERROR, "Invalid cursor id: %d\n", currentCursor);
+		currentCursor = baseCursor;
+	}
+
 	SDL_SetCursor(cursors[currentCursor].get());
-	//CursorChanged = false;
 }
 
 void Cursor::setCursor(int num)
 {
 	currentCursor = num;
 	CursorChanged = true;
-	//UpdateCursor();
 }
 
-//TODO: not needed
 void Cursor::resetCursor()
 {
 	currentCursor = baseCursor;
-	//UpdateCursor();
 }
--- source/Engine/Cursor.h
+++ source/Engine/Cursor.h
@@ -13,7 +13,11 @@ public:
 	static bool CursorChanged;
 
 private:
-	static SDL_Cursor_ptr cursors[20];
+	static SDL_Cursor_ptr loadCursorFromSheet(std::string filename, SDL_Rect rect);
+
+	static SDL_Surface_ptr spriteSheet;
+	static std::string spriteSheetPath;
+	static SDL_Cursor_ptr cursors[30];
 	static int currentCursor;
 	static int baseCursor;
 };
